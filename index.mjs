50f39e29262c4840d0612a7e7ae7219fb4ac83de
DjKesu <km67000@yahoo.com>
2023-07-20 19:26:44 -0400

    Added react app creation

--- index.mjs
+++ index.mjs
@@ -1,20 +1,64 @@
 import inquirer from "inquirer";
 import { Configuration, OpenAIApi } from "openai";
+import execa from "execa";
+import fs from "fs-extra";
+import { exit } from "process";
+import ora from "ora";
+
+async function createReactApp(projectName) {
+  try {
+    const spinner = ora("Creating React app").start();
+    const { stdout } = await execa("npx", ["create-react-app", projectName]);
+    spinner.succeed("React app created successfully.");
+    return stdout;
+  } catch (error) {
+    if (error.stdout.includes("conflict")) {
+      return "React app already exists. Can't overwrite. Retry with a different name.";
+    }
+    return error.shortMessage;
+  }
+}
+
+async function modifyComponentFile(
+  projectName,
+  componentName,
+  generatedContent
+) {
+  const componentFilePath = `./${projectName}/src/${componentName}.js`;
+  try {
+    // Read the existing component file
+    let componentFileContent = await fs.readFile(componentFilePath, "utf8");
+
+    // Modify the content (replace a placeholder with the generated content)
+    const placeholder = "// GPT_GENERATED_CONTENT";
+    componentFileContent = componentFileContent.replace(
+      placeholder,
+      generatedContent
+    );
+
+    // Write the modified content back to the file
+    await fs.writeFile(componentFilePath, componentFileContent, "utf8");
+
+    console.log(`Modified ${componentName}.js with generated content.`);
+  } catch (error) {
+    console.error("Error modifying component file:", error.message);
+  }
+}
+
+async function installDependencies(projectName) {
+  try {
+    // Change the working directory to the React app project
+    process.chdir(projectName);
+
+    // Install dependencies using npm
+    await execa("npm", ["install"]);
+    console.log("Dependencies installed successfully.");
+  } catch (error) {
+    console.error("Error installing dependencies:", error.message);
+  }
+}
 
 async function createInstance(apiKey, orgId) {
-  // Input checks
-  if (typeof apiKey !== "string" || apiKey.length !== 64) {
-    throw new Error(
-      "Invalid API key format. Please provide a valid OpenAI API key."
-    );
-  }
-
-  if (typeof orgId !== "string" || orgId.length === 0) {
-    throw new Error(
-      "Invalid organization ID format. Please provide a valid OpenAI organization ID."
-    );
-  }
-  console.log("Input checks passed");
   console.log("Creating OpenAI API instance");
   const openAiConfig = new Configuration({
     organization: orgId,
@@ -53,13 +97,20 @@ async function promptUser() {
   ]);
   console.log(projectName, prompt, apiKey, orgId);
 
-  // Create OpenAI API instance
   try {
     // Create OpenAI API instance
     const openAiApi = await createInstance(apiKey, orgId);
-    console.log(openAiApi);
+    // console.log(openAiApi);
   } catch (error) {
     console.error("Error creating OpenAI API instance:", error.message);
+    exit(1);
+  }
+
+  // Create React app
+  try {
+    const createReactAppResult = await createReactApp(projectName);
+  } catch (error) {
+    console.error("Error creating React app:", error.message);
   }
 }
 
